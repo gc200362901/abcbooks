b5e9f47cfff8f60faa93bacd01c488b942d6634e
gc200362901 <200362901@student.georgianc.on.ca>
2018-08-02 22:29:15 -0400

    show/edit/delete

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -26,6 +26,41 @@ exports.new = function(req, res, next) {
     res.render('books/new', locals)
 };
 
+// VIEW books/12345
+exports.show = function(req, res, next) {
+    let locals = {
+        title: "Book"
+    }
+
+    Book.findById({
+        _id: req.params.id
+    })
+    .then(function(book) {
+        locals.book = book;
+        res.render('books/show', locals)
+    })
+    .catch(function(err) {
+        next(err)
+    })
+};
+
+// VIEW books/12345/edit
+exports.edit = function(req, res, next) {
+    let locals = {
+        title: 'Edit Book'
+    }
+    Book.findById({
+        _id: req.params.id
+    })
+    .then(function(book) {
+        locals.book = book;
+        res.render('books/edit', locals)
+    })
+    .catch(function(err){
+        next(err)
+    })
+};
+
 // create
 exports.create = function(req, res, next) {
     if ( req.files && req.files.image ) {
@@ -49,6 +84,51 @@ exports.create = function(req, res, next) {
         res.redirect('/books')
     })
     .catch(function(err){
+        next(err)
+    })
+  };
+
+  //update
+  exports.update = function(req, res, next) {
+    if ( req.files && req.files.image ) {
+        let image = req.files.image
+        image.mv(`public/images/books/${image.name}`)
+        imageName = image.name;
+      } 
+      else {
+        imageName = 'defaultBook.jpg';
+      }
+      Book.findById(req.params.id)
+      .then(function(book) {
+        book.title = req.body.title,
+        book.author = req.body.author,
+        book.year = req.body.year,
+        book.description = req.body.description,
+        book.price = req.body.price,
+        book.image = imageName
+
+        book.save()
+        .then(function() {
+            res.redirect('/books/' + book.id)
+        })
+        .catch(function(err) {
+            next(err)
+        })
+      })
+      .catch(function(err) {
+          next(err)
+      })
+  };
+
+  // delete
+  exports.delete = function(req, res) {
+    Book.remove({
+        _id: req.body.id
+    })
+    .then(function() {
+        res.redirect('/books')
+    })
+    .catch(function(err) {
         next(err)
     })
   };
--- routes/books.js
+++ routes/books.js
@@ -9,7 +9,19 @@ router.get('/', booksController.index);
 // GET /books/new
 router.get('/new', booksController.new);
 
+// GET /books/12345
+router.get( '/:id', booksController.show );
+
+// GET /books/12345/edit
+router.get('/:id/edit', booksController.edit);
+
 // POST create
 router.post('/', booksController.create);
+
+// POST /books/12345
+router.post('/:id', booksController.update);
+
+// POST books/12345/delete
+router.post('/:id/delete', booksController.delete);
 
 module.exports = router;
